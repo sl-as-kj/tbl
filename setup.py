c600e4218f3beefc4d896d6dbaaba6b478d82570
Alex Samuel <alex@alexsamuel.net>
2017-05-31 23:53:53 -0400

    No Python 2.

--- setup.py
+++ setup.py
@@ -22,8 +22,6 @@ setuptools.setup(
 
         "License :: OSI Approved :: MIT License",
 
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
     ],
