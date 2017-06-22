21663800c4a84189f41fe6f632a5c4c34ef44afd
Alex Samuel <alex@alexsamuel.net>
2017-06-22 15:14:49 -0400

    Rename State to View.

--- tbl/test/test_layout.py
+++ tbl/test/test_layout.py
@@ -2,7 +2,7 @@ from   collections import OrderedDict as odict
 import numpy as np
 
 from   tbl.screen import Model
-from   tbl.view import State, lay_out_columns
+from   tbl.view import View, lay_out_columns
 
 #-------------------------------------------------------------------------------
 
@@ -10,12 +10,12 @@ def test_lay_out_0():
     model = Model()
     model.add_col(np.array([ 1,  2,  3]), "foo")
     model.add_col(np.array([ 8,  9, 10]), "bar")
-    state = State(model)
-    state.left_border   = "|>"
-    state.separator     = "||"
-    state.right_border  = "<|"
+    view = View(model)
+    view.left_border   = "|>"
+    view.separator     = "||"
+    view.right_border  = "<|"
 
-    assert [ tuple(l) for l in lay_out_columns(state) ] == [
+    assert [ tuple(l) for l in lay_out_columns(view) ] == [
         ( 0, 2, "text", "|>"),
         ( 2, 3, "col", 0),
         ( 5, 2, "text", "||"),
